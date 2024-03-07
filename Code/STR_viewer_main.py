a8517b8337096b81d6fc58a0604e59c509b8f509
Guy baz <157303287+G-S-Baz@users.noreply.github.com>
2024-03-07 13:44:02 +0200

    deleting optional code that was taken out by inserting a note sign

--- Code/STR_viewer_main.py
+++ Code/STR_viewer_main.py
@@ -12,26 +12,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# import sys
-# from PyQt6 import QtWidgets
-# from STRviewerUIElements import STRViewerUI
-# from STRviewerLogic import STRViewerLogic
-
-# # Main part of the script
-# def main():
-#     qapp = QtWidgets.QApplication.instance() or QtWidgets.QApplication(sys.argv)
-
-#     # Create UI elements
-#     ui_elements = STRViewerUI()
-
-#     # Create logic class and pass UI elements
-#     logic = STRViewerLogic(ui_elements)
-
-#     # Show the UI
-#     ui_elements.show()
-
-#     qapp.exec()
-
-# if __name__ == "__main__":
-#     main()
